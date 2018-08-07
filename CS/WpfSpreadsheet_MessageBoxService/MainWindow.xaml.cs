24ea59362a07882083387a889cc73d099f9ed892
Natalya Senichkina <29920736+NatalyaSenichkina@users.noreply.github.com>
2018-08-07 17:21:58 +0300

    add example content

---
+++ CS/WpfSpreadsheet_MessageBoxService/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfSpreadsheet_MessageBoxService
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : DevExpress.Xpf.Core.ThemedWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void spreadsheet_Loaded(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                ReplaceMessageBoxService();
+            }), System.Windows.Threading.DispatcherPriority.Render);
+        }
+
+        private void ReplaceMessageBoxService()
+        {
+            // Replace the default MessageBoxService service with a custom one.
+            MyMessageBoxService service = new MyMessageBoxService();
+            spreadsheet.ReplaceService<DevExpress.XtraSpreadsheet.Services.IMessageBoxService>(service);
+        }
+    }
+}
---
+++ CS/WpfSpreadsheet_MessageBoxService/MyMessageBoxService.cs
@@ -0,0 +1,59 @@
+﻿using DevExpress.Spreadsheet;
+using DevExpress.Xpf.Core;
+using DevExpress.XtraSpreadsheet.Localization;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace WpfSpreadsheet_MessageBoxService
+{
+    public class MyMessageBoxService : DevExpress.XtraSpreadsheet.Services.IMessageBoxService
+    {
+        const string myMessage = "This is a custom message box.\n\n";
+        // To test: enter incorrect data into cells with a data validation rule applied. 
+        public DialogResult ShowDataValidationDialog(string message, string title, DataValidationErrorStyle errorStyle)
+        {
+            string customMessage = myMessage + message;
+            if (errorStyle == DataValidationErrorStyle.Stop)
+            {
+                MessageBoxResult result = DXMessageBox.Show(customMessage, title, MessageBoxButton.OKCancel, MessageBoxImage.Stop);
+                return result == MessageBoxResult.OK ? DialogResult.No : (DialogResult)result;
+            }
+            if (errorStyle == DataValidationErrorStyle.Warning)
+                return (DialogResult)DXMessageBox.Show(customMessage, title, MessageBoxButton.YesNoCancel, MessageBoxImage.Warning);
+            return (DialogResult)DXMessageBox.Show(customMessage, title, MessageBoxButton.OKCancel, MessageBoxImage.Information);
+        }
+
+        // To test: set the row height to an invalid value.
+        public DialogResult ShowMessage(string message, string title, MessageBoxIcon icon)
+        {
+            string customMessage = myMessage + message;
+            DXMessageBox.Show(customMessage, title, MessageBoxButton.OK, (MessageBoxImage)icon);
+            return DialogResult.OK;
+        }
+
+        // To test: select cells with different data validation rules applied and click Data Validation to invoke the Data Validation dialog.
+        public bool ShowOkCancelMessage(string message)
+        {
+            string customMessage = myMessage + message;
+            // Hide the 'Do you want to replace the contents of the destination cells?' message.
+            // This message appears when you overwrite the existing cell content (e.g., when you drag and drop cells to another location).
+            if (message == XtraSpreadsheetLocalizer.GetString(XtraSpreadsheetStringId.Msg_CanReplaceTheContentsOfTheDestinationCells))
+                return true;
+            return DXMessageBox.Show(customMessage, System.Windows.Forms.Application.ProductName, MessageBoxButton.OKCancel, MessageBoxImage.Warning) == MessageBoxResult.OK;
+        }
+
+        // To test: select a cell range with and without data validation settings and click Data Validation to invoke the Data Validation dialog.
+        public DialogResult ShowYesNoCancelMessage(string message)
+        {
+            string customMessage = myMessage + message;
+            return (DialogResult)DXMessageBox.Show(customMessage, System.Windows.Forms.Application.ProductName, MessageBoxButton.YesNoCancel, MessageBoxImage.Information);
+        }
+
+        // To test: delete a defined name in the Name Manager dialog.
+        public bool ShowYesNoMessage(string message)
+        {
+            string customMessage = myMessage + message;
+            return DXMessageBox.Show(customMessage, System.Windows.Forms.Application.ProductName, MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes;
+        }
+    }
+}
