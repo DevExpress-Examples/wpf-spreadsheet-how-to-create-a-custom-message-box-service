6cb32ab49c5ee9d634f2c077c52961cd9cf487bc
Natalya Senichkina <29920736+NatalyaSenichkina@users.noreply.github.com>
2018-08-08 12:04:03 +0300

    Update VB and Readme.md

--- VB/WpfSpreadsheet_MessageBoxService/MyMessageBoxService.vb
+++ VB/WpfSpreadsheet_MessageBoxService/MyMessageBoxService.vb
@@ -3,34 +3,35 @@ Imports DevExpress.Xpf.Core
 Imports DevExpress.XtraSpreadsheet.Localization
 Imports System.Windows
 Imports System.Windows.Forms
+Imports DevExpress.XtraSpreadsheet.Services
 
 Namespace WpfSpreadsheet_MessageBoxService
     Public Class MyMessageBoxService
-        Implements DevExpress.XtraSpreadsheet.Services.IMessageBoxService
+        Implements IMessageBoxService
 
         Private Const myMessage As String = "This is a custom message box." & vbLf & vbLf
         ' To test: enter incorrect data into cells with a data validation rule applied. 
-        Public Function ShowDataValidationDialog(ByVal message As String, ByVal title As String, ByVal errorStyle As DataValidationErrorStyle) As DialogResult
+        Public Function ShowDataValidationDialog(ByVal message As String, ByVal title As String, ByVal errorStyle As DataValidationErrorStyle) As DialogResult Implements IMessageBoxService.ShowDataValidationDialog
             Dim customMessage As String = myMessage & message
-            If errorStyle Is DataValidationErrorStyle.Stop Then
+            If errorStyle = DataValidationErrorStyle.Stop Then
                 Dim result As MessageBoxResult = DXMessageBox.Show(customMessage, title, MessageBoxButton.OKCancel, MessageBoxImage.Stop)
                 Return If(result = MessageBoxResult.OK, DialogResult.No, CType(result, DialogResult))
             End If
-            If errorStyle Is DataValidationErrorStyle.Warning Then
+            If errorStyle = DataValidationErrorStyle.Warning Then
                 Return CType(DXMessageBox.Show(customMessage, title, MessageBoxButton.YesNoCancel, MessageBoxImage.Warning), DialogResult)
             End If
             Return CType(DXMessageBox.Show(customMessage, title, MessageBoxButton.OKCancel, MessageBoxImage.Information), DialogResult)
         End Function
 
         ' To test: set the row height to an invalid value.
-        Public Function ShowMessage(ByVal message As String, ByVal title As String, ByVal icon As MessageBoxIcon) As DialogResult
+        Public Function ShowMessage(ByVal message As String, ByVal title As String, ByVal icon As MessageBoxIcon) As DialogResult Implements IMessageBoxService.ShowMessage
             Dim customMessage As String = myMessage & message
             DXMessageBox.Show(customMessage, title, MessageBoxButton.OK, CType(icon, MessageBoxImage))
             Return DialogResult.OK
         End Function
 
         ' To test: select cells with different data validation rules applied and click Data Validation to invoke the Data Validation dialog.
-        Public Function ShowOkCancelMessage(ByVal message As String) As Boolean
+        Public Function ShowOkCancelMessage(ByVal message As String) As Boolean Implements IMessageBoxService.ShowOkCancelMessage
             Dim customMessage As String = myMessage & message
             ' Hide the 'Do you want to replace the contents of the destination cells?' message.
             ' This message appears when you overwrite the existing cell content (e.g., when you drag and drop cells to another location).
@@ -41,13 +42,13 @@ Namespace WpfSpreadsheet_MessageBoxService
         End Function
 
         ' To test: select a cell range with and without data validation settings and click Data Validation to invoke the Data Validation dialog.
-        Public Function ShowYesNoCancelMessage(ByVal message As String) As DialogResult
+        Public Function ShowYesNoCancelMessage(ByVal message As String) As DialogResult Implements IMessageBoxService.ShowYesNoCancelMessage
             Dim customMessage As String = myMessage & message
             Return CType(DXMessageBox.Show(customMessage, System.Windows.Forms.Application.ProductName, MessageBoxButton.YesNoCancel, MessageBoxImage.Information), DialogResult)
         End Function
 
         ' To test: delete a defined name in the Name Manager dialog.
-        Public Function ShowYesNoMessage(ByVal message As String) As Boolean
+        Public Function ShowYesNoMessage(ByVal message As String) As Boolean Implements IMessageBoxService.ShowYesNoMessage
             Dim customMessage As String = myMessage & message
             Return DXMessageBox.Show(customMessage, System.Windows.Forms.Application.ProductName, MessageBoxButton.YesNo, MessageBoxImage.Warning) = MessageBoxResult.Yes
         End Function
